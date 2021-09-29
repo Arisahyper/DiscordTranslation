1d768b0dd11abf38cbe0dba7586d47f2d6683f13
thr <50509145+Arisahyper@users.noreply.github.com>
2021-09-29 19:49:03 +0900

    Remove unnecessary comments and format the code.

--- main.py
+++ main.py
@@ -5,52 +5,32 @@ import json
 json_open = open('password.json', 'r')
 json_load = json.load(json_open)
 
-# シークレットトークンの定義
 token = json_load['discord']['token']
 
-# 略
 client = discord.Client()
 translator = Translator()
 
-# サーバーIDの取得
-guild = client.get_guild(841936352077414410) # microsoft dev server
-
-
-
+guild = client.get_guild(841936352077414410)
 
 @client.event
 async def on_ready():
-    # 起動時メッセージ
     print("run")
 
 @client.event
 async def on_message(message):
-    # botの発言には反応しない
     if message.author.bot:
         return
 
-    # 言語判別
     lang = translator.detect(message.content).lang
 
-    # if 言語が日本語だったら
     if lang == "ja":
-        #チャンネル指定
-        channel = client.get_channel(841949669248598046) # en-text-manage
-        # 変換
+        channel = client.get_channel(841949669248598046)
         trans_text = translator.translate(message.content, src = lang, dest = "en").text
-        # 送信
         await channel.send(trans_text)
 
-    # if 言語が英語だったら
     elif lang == "en":
-        #チャンネル指定
-        channel = client.get_channel(841949627142111232) # ja-text-manage
-        # 変換
+        channel = client.get_channel(841949627142111232)
         trans_text = translator.translate(message.content, src = lang, dest = "ja").text
-        # 送信
         await channel.send(trans_text)
-
-
-
 
 client.run(token)
